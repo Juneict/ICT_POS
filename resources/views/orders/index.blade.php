33a43f399204d1a6cf2dc5ec67d335ff1491a66d
DESKTOP-12EEBOJ\JuneMoeNyiNyi <junemoenyinyiict@gmail.com>
2022-01-23 16:35:23 +0630

    after cashier function add

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -3,9 +3,7 @@
 @section('content')
 <style>
 
-select.form-control {
-    -webkit-appearance: menulist;
-}
+
     .modal.right .modal-dialog{
         
         top:0;
@@ -18,10 +16,10 @@ select.form-control {
     }
 </style>
 
-    <div class="container">
+    <div class="container-fluid">
         <div class="col-md-12">
             <div class="row">
-               <div class="col-md-9">
+               <div class="col-md-8">
                     <div class="card">
                     @if (session('Success'))
                         <div class="alert alert-success">
@@ -36,7 +34,10 @@ select.form-control {
                             <div class="card-header">
                                 <h4 style="float:left">Products</h4>
                                 <a href="" class="btn btn-dark" style="float:right" data-toggle="modal" data-target="#addproduct">
-                                <i class="fa fa-plus"></i>Add New Product</a></div>
+                                <i class="fa fa-plus"></i>Add New Product</a>
+                            </div>
+                            <form action="{{ route('orders.store')}}" method="post">
+                                @csrf
                             <div class="card-body">
                                 <table class="table  table-left">
                                         <thead>
@@ -54,7 +55,7 @@ select.form-control {
                                             <tr>
                                                 <td>1</td>
                                                 <td>
-                                                    <select name="product_id" id="product_id" class="form-control product_id">
+                                                    <select name="product_id[]" id="product_id" class="form-control product_id">
                                                         <option value="">Select items</option>
                                                         @foreach($products as $product)
                                                             <option data-price="{{$product->price}}" value="{{$product->id}}">{{$product->product_name}}</option>
@@ -82,14 +83,61 @@ select.form-control {
                             </div>
                         </div>
                </div>
-               <div class="col-md-3">
+               <div class="col-md-4">
                     <div class="card">
                             <div class="card-header"><h4>Total <b class="total"></b> mmk</h4></div>
                             <div class="card-body">
-                                
+                                <div class="panel">
+                                    <div class="row">
+                                        <table class="table table-striped">
+                                            <tr>
+                                                <td>
+                                                    <label for="">Customer Name</label>
+                                                    <input type="text" class="form-control" name="customer_name">
+                                                </td>
+                                                <td>
+                                                    <label for="">Customer Phone</label>
+                                                    <input type="text" class="form-control" name="customer_phone">
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        <td>Payment
+                                            <span class="radio-item">
+                                                <input type="radio" name="payment_method" id="payment_method" class="true" value="Cash" checked="checked">
+                                                <label for="payment_method"><i class="fa fa-money-bill text-success"></i>Cash</label>
+                                            </span>
+                                            <span class="radio-item">
+                                                <input type="radio" name="payment_method" id="payment_method" class="true" value="BankTransfer">
+                                                <label for="payment_method"><i class="fa fa-university text-danger"></i>Bank</label>
+                                            </span>
+                                            <span class="radio-item">
+                                                <input type="radio" name="payment_method" id="payment_method" class="true" value="Credit-Card" >
+                                                <label for="payment_method"><i class="fa fa-credit-card text-info"></i>Credit-Card</label>
+                                            </span>
+                                        </td>
+                                        <td>
+                                            Payment 
+                                            <input type="number" name="paid_amount" id="paid_amount" class="form-control">
+                                        </td>
+                                        <td>
+                                            Returning Change 
+                                            <input type="number" name="balance" id="balance" class="form-control" readonly>
+                                        </td>
+                                        <td>
+                                            <button class="btn-primary btn-lg btn-block mt-3">Save</button>
+                                        </td>
+                                        <td>
+                                            <button class="btn-danger btn-lg btn-block mt-3">Calculator</button>
+                                        </td>
+                                        <td>
+                                            <a href="" class="text-center mt-3"><i class="fa fa-sign-out"></i> logout</a>
+                                        </td>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                </div>
+                </form>
             </div>
         </div>
     </div>
@@ -189,16 +237,22 @@ select.form-control {
               TotalAmount();
            })
 
-           $('.addMoreProduct').delegate('.quantity,.discount','click',function(e){
-            e.preventDefault();
+           $('.addMoreProduct').delegate('.quantity,.discount','click',function(){
+           
               var tr = $(this).parent().parent();
-             
+              var product_id = tr.find('.product_id');
               var qty = tr.find('.quantity').val() - 0;
               var disc = tr.find('.discount').val() - 0;
               var price = tr.find('.price').val() - 0 ;
               var total_amount =(qty*price)-((qty*price*disc) / 100);
               tr.find('.total_amount').val(total_amount);
               TotalAmount();
+           })
+           $('#paid_amount').keyup(function(){
+               var total =$('.total').html();
+               var paid_amount =$(this).val();
+               var tot = paid_amount- total;
+               $('#balance').val(tot).toFixed;
            })
         </script>
 @endsection
